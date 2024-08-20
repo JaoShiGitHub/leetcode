498f6ff68192b61040b0dcc5ae9467acf3e8c02a
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-20 23:58:04 +0700

    Create counter-2.js

---
+++ 30DJS/closures/counter-2.js
@@ -0,0 +1,23 @@
+/*
+ 2665 Counter II
+
+ https://leetcode.com/problems/counter-ii/description/?envType=study-plan-v2&envId=30-days-of-javascript
+
+ Write a function createCounter. It should accept an initial integer init. 
+ It should return an object with three functions.
+
+ The three functions are:
+
+ increment() increases the current value by 1 and then returns it.
+ decrement() reduces the current value by 1 and then returns it.
+ reset() sets the current value to init and then returns it. */
+
+const createCounter = init => {
+  let currentValue = init;
+
+  return {
+    increment: () => ++currentValue,
+    decrement: () => --currentValue,
+    reset: () => currentValue = init
+  };
+};
