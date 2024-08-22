3ec6ac9b0837a41eeaf5736dff1b24e7d6bf71a5
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-22 20:13:32 +0700

    Add title and link

--- 30DJS/2-basic-array-transformations/3-array-reduce-transformation.js
+++ 30DJS/2-basic-array-transformations/3-array-reduce-transformation.js
@@ -1,5 +1,8 @@
 /*
 
+2626. Array Reduce Transformation : EASY
+https://leetcode.com/problems/array-reduce-transformation/?envType=study-plan-v2&envId=30-days-of-javascript
+
 the final result obtained by executing the fn function on each element of the array, 
 sequentially, passing in the return value from the calculation on the preceding element.
 
