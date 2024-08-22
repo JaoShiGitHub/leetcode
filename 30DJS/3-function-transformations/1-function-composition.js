f188d082ca6f95f8f8f7d35f7ec44af4c890810c
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-22 20:26:17 +0700

    Create 1-function-composition.js

---
+++ 30DJS/3-function-transformations/1-function-composition.js
@@ -0,0 +1,22 @@
+/*
+
+2629. Function Composition : EASY
+https://leetcode.com/problems/function-composition/?envType=study-plan-v2&envId=30-days-of-javascript
+
+Given an array of functions [f1, f2, f3, ..., fn], 
+return a new function fn that is the function composition of the array of functions.
+
+The function composition of [f(x), g(x), h(x)] is fn(x) = f(g(h(x))).
+
+The function composition of an empty list of functions is the identity function f(x) = x.
+
+You may assume each function in the array accepts one integer as input and returns one integer as output.
+
+*/
+
+const compose = function(functions) {
+    const fn = functions.reverse()
+    return function(x) {
+        
+    }
+};
