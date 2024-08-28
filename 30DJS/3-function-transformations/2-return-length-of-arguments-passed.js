82d833ffc364836e349aaac0a4266d791f13e893
OSHIN <158509127+JaoShiGitHub@users.noreply.github.com>
2024-08-28 22:02:52 +0700

    Create 2-return-length-of-arguments-passed.js

---
+++ 30DJS/3-function-transformations/2-return-length-of-arguments-passed.js
@@ -0,0 +1,11 @@
+/*
+    2703. Return Length of Arguments Passed    |    EASY
+    https://leetcode.com/problems/return-length-of-arguments-passed/description/?envType=study-plan-v2&envId=30-days-of-javascript
+    
+    Write a function argumentsLength that returns the count of arguments passed to it.
+    
+*/
+
+const argumentsLength = function(...args) {
+    return args.length;
+};
